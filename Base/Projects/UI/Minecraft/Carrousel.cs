78912ed22d0317902a6c2c6603ec88b43bd9c6b0
N04H <ououn@outlook.fr>
2025-05-02 16:24:16 +0200

    Added slider text change

---
+++ Base/Projects/UI/Minecraft/Carrousel.cs
@@ -0,0 +1,90 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+
+public class Carrousel : MonoBehaviour
+{
+    public int index = -1;
+    [SerializeField] private string[] stringArray;
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        if (stringArray == null || stringArray.Length == 0)
+        {
+            Debug.LogError("String array is not set or empty");
+            return;
+        }
+        index = 0; // Initialize index to the first element
+    }
+
+    public void Next()
+    {
+        index++;
+        if (index >= stringArray.Length)
+        {
+            index = 0;
+        }
+        Debug.Log(stringArray[index]);
+        SetTextMeshProUGUI();
+    }
+
+    public void Previous()
+    {
+        index--;
+        if (index < 0)
+        {
+            index = stringArray.Length - 1;
+        }
+        Debug.Log(stringArray[index]);
+        SetTextMeshProUGUI();
+    }
+
+    public void SetIndex(int i)
+    {
+        if (i >= 0 && i < stringArray.Length)
+        {
+            index = i;
+            Debug.Log(stringArray[index]);
+            SetTextMeshProUGUI();
+        }
+        else
+        {
+            Debug.LogError("Index out of range");
+        }
+    }
+
+    public void SetIndex(float i)
+    {
+        if (i >= 0 && i < stringArray.Length)
+        {
+            index = (int)i;
+            Debug.Log(stringArray[index]);
+            SetTextMeshProUGUI();
+        }
+        else
+        {
+            Debug.LogError("Index out of range");
+        }
+    }
+    public int GetIndex()
+    {
+        return index;
+    }
+    public string GetString()
+    {
+        return stringArray[index];
+    }
+    public void SetTextMeshProUGUI()
+    {
+        if (text != null)
+        {
+            text.text = stringArray[index];
+        }
+        else
+        {
+            Debug.LogError("TextMeshProUGUI is not set");
+        }
+    }
+}
